60d34a6b71f34220f5c2c73f14dd81330af682d0
s2drag0n <2558058352@qq.com>
2024-11-20 13:08:01 +0800

    default

---
+++ mkdotfile.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+cp -f ~/.zshrc ~/dotfile/.zshrc
+cp -r -f ~/script/ ~/dotfile/
+cp -r -f ~/.config/alacritty/ ~/dotfile/.config/
+cp -r -f ~/.config/i3/ ~/dotfile/.config/
+cp -r -f ~/.config/nvim/ ~/dotfile/.config/
+cp -r -f ~/.config/picom/ ~/dotfile/.config/
---
+++ todo.md
@@ -0,0 +1,5 @@
+0. sysytem English done
+0. Caplock -> Ctrl done
+0. terminal Chinese input problem done
+
+1. brightness adjustment
